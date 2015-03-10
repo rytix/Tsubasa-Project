7769a3f14a4425ec7880f163f616308b53e8680e
unknown <4289714@LOC1071-004670.univali.br>
2015-03-09 21:50:30 -0300

    Adicionado as ultimas classes de dominio

---
+++ application/models/Diretor.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Diretor
+ *
+ * @author 4289714
+ */
+class Diretor extends Usuario {
+
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    public function criarCampeonato(){
+        //TODO Logica de criação de campeonato
+    }
+
+}
---
+++ application/models/campeonato.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of campeonato
+ *
+ * @author Paulo Eduardo Martins
+ */
+class Campeonato extends CI_Model {
+
+    private $nome;
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function gerarTabelaPontuacao() {
+        //TODO funcao gerar tabela pontuação
+    }
+    /**
+     * 
+     * @return String
+     */
+    public function getNome() {
+        return $this->nome;
+    }
+    /**
+     * 
+     * @param String $nome
+     * @return Campeonato
+     */
+    public function setNome($nome) {
+        if (FALSE === is_string($nome)) {
+            $tipoEncontradoErro = gettype($nome);
+            if ($tipoEncontradoErro == 'object') {
+                $tipoEncontradoErro = get_class($nome);
+            }
+            trigger_error('$nome precisa ser uma string, encontrado:' . $tipoEncontradoErro, E_USER_ERROR);
+        }
+
+        $this->nome = $nome;
+        return $this;
+    }
+
+}
