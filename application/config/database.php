68792d3049147d263d450044e9a8ca1fe9deb45a
unknown <4276663@LOC1071-004734.univali.br>
2015-03-16 19:36:30 -0300

    database config

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'postgres';
+$db['default']['password'] = '123';
+$db['default']['database'] = 'tsubasa_db';
 $db['default']['dbdriver'] = 'postgre';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
