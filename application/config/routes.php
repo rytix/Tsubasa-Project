4a46de057e3074490718d49df69f004134ef6368
leolap <leolap8@gmail.com>
2015-03-27 09:51:39 -0300

    Revert "adicionado index.php que alguém removeu."
    This reverts commit d19aedda1ca00aedba428045928717941e835308.

--- application/config/routes.php
+++ application/config/routes.php
@@ -39,8 +39,8 @@
 */
 
 //$route['404_override'] = '';
-//$route['index.php/juiz/cadastrojuiz'] = 'index.php/juiz/cadastrojuiz';
-//$route['index.php/juiz'] = 'index.php/juiz';
+$route['index.php/juiz/cadastrojuiz'] = 'index.php/juiz/cadastrojuiz';
+$route['index.php/juiz'] = 'index.php/juiz';
 $route['default_controller'] = 'welcome/index';
 
 /* End of file routes.php */
