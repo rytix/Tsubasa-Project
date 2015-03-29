c6bde1a6f27c74ead5e7f202d74713976febeed6
Rytix <aff12300@gmail.com>
2015-03-29 20:51:04 -0300

    head padrão (não o header, o head só)

---
+++ application/views/head.php
@@ -0,0 +1,51 @@
+<?php $this->load->helper('url'); ?>
+<?php $this->load->helper('form'); ?>
+<?php $this->load->library('form_validation'); ?>
+<script type="text/javascript" src="http://localhost/Tsubasa-Project/application/libraries/js/jquery-2.1.1.min.js"></script>
+<link rel="stylesheet" href="http://localhost/Tsubasa-Project/application/libraries/css/bootstrap.min.css">    
+<link rel="stylesheet" href="http://localhost/Tsubasa-Project/application/libraries/css/datepicker.css">
+<link rel="stylesheet" href="http://localhost/Tsubasa-Project/application/libraries/css/datepicker3.css">
+<link rel="stylesheet" href="http://localhost/Tsubasa-Project/application/libraries/css/font-awesome/css/font-awesome.min.css">
+<meta charset="UTF-8">
+<style type="text/css">
+    body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #eee;
+    }
+
+    .form-signin {
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+    }
+    .form-signin .form-signin-heading,
+    .form-signin .checkbox {
+        margin-bottom: 10px;
+    }
+    .form-signin .checkbox {
+        font-weight: normal;
+    }
+    .form-signin .form-control {
+        position: relative;
+        height: auto;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        padding: 10px;
+        font-size: 16px;
+    }
+    .form-signin .form-control:focus {
+        z-index: 2;
+    }
+    .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+    .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+    }
+</style>
