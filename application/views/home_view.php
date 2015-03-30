986bdd4af2e966b3384d2c1987ced1b3bf62c2ee
Rytix <aff12300@gmail.com>
2015-03-30 00:23:51 -0300

    login com view basica

---
+++ application/views/home_view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>  
+<html>
+    <head>
+        <?php include('head.php'); ?>
+        <title>CT Login</title>    
+    </head>
+    <body>
+        <?php include('header.php') ?>
+        <div class="container">
+            <div class="row">
+                
+            </div>
+        </div>
+    </body>
+</html>
